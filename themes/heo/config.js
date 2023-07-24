9986283201765e86daa616b24fc98f303f271a2b
sg-zzy <63586271+sg-zzy@users.noreply.github.com>
2023-07-24 18:59:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,8 +14,8 @@ const CONFIG = {
   HERO_TITLE_2: '与思维认知',
   HERO_TITLE_3: 'TANGLY1024.COM',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_5: '访问博客',
+  HERO_TITLE_LINK: 'https://notion-next-nu-puce.vercel.app/archive?theme=heo',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
